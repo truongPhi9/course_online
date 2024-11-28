11db7a23808f15b2c054d67575501b07223737f9
truongphi <truongphi754@gmail.com>
2024-11-28 09:42:16 +0700

    udpate

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import './App.css';
-import Header from './components/Header';
-import { Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react';
+import "./App.css";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 // import axios from 'axios';
-import Menu from './components/Menu'
-import Home from './page/Home';
-import Footer from './components/Footer';
+import Menu from "./components/Menu";
+import Home from "./page/Home";
+import Footer from "./components/Footer";
 
 // import page website
-import Learn from './page/Learn';
-import LearningPath from './page/LearningPath';
-import CourseDetail from './page/CourseDetail';
-import BlogDetail from './page/BlogDetail';
-import Blog from './page/Blog';
-import Login from './page/Login';
-import Admin from './admin/Admin';
-import From from './components/From';
-import Edit from './components/Edit'
+import Learn from "./page/Learn";
+import LearningPath from "./page/LearningPath";
+import CourseDetail from "./page/CourseDetail";
+import BlogDetail from "./page/BlogDetail";
+import Blog from "./page/Blog";
+import Login from "./page/Login";
+import Admin from "./admin/Admin";
+import From from "./components/From";
+import Edit from "./components/Edit";
 
 function App() {
   const [check, setCheck] = useState(false);
-  const [checkAdmin, setCheckAdmin] = useState(false)
+  const [checkAdmin, setCheckAdmin] = useState(false);
 
-  const isLoginPage = window.location.href.split('/')[3] === 'login';
-  const isAdminPage = window.location.href.split('/')[3] === 'admin';
+  const isLoginPage = window.location.href.split("/")[3] === "login";
+  const isAdminPage = window.location.href.split("/")[3] === "admin";
   // console.log(isAdminPage);
   useEffect(() => {
     setCheck(isLoginPage);
     setCheckAdmin(isAdminPage);
-  }, [isLoginPage, isAdminPage])
+  }, [isLoginPage, isAdminPage]);
 
   return (
     <>
       {!check ? <Header checkAdmin={checkAdmin} /> : null}
-      <div className='flex min-w-screen'>
-        <div className='sticky top-[74px] z-30'>
-        {!check ? <Menu checkAdmin={checkAdmin} /> : null}
+      <div className="flex min-w-screen">
+        <div className="sticky top-[74px] z-30">
+          {!check ? <Menu checkAdmin={checkAdmin} /> : null}
         </div>
-        <div className={`w-full ${!check ? "max-w-screen-page" : ''} px-5 mt-5 relative`}>
+        <div
+          className={`w-full ${
+            !check ? "max-w-screen-page" : ""
+          } px-5 mt-5 relative`}
+        >
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/learning-path" element={<LearningPath />} />
